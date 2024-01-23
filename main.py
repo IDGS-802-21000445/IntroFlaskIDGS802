1739d9cf9b4757805c19187a1756a13b80fb2b74
EdwinY-a <yovanirivers08@gmail.com>
2024-01-22 21:07:26 -0600

    EYRR - 22 ENE 2024 - Layout

--- main.py
+++ main.py
@@ -1,10 +1,73 @@
-from flask import Flask
+from flask import Flask,request,render_template
 
 app = Flask(__name__)
 
 @app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/alumnos")
+def alumnos():
+    return render_template("alumnos.html")
+
+@app.route("/maestros")
+def maestros():
+    return render_template("maestros.html")
+
+@app.route("/datos")
 def hola():
     return "<p> Hola Mundo <p>"
 
+@app.route("/hola")
+def func():
+    return "<h1> Saludo desde UTL </h1>"
+
+@app.route("/saludo")
+def func1():
+    return "<h1> Saludo desde Saludo </h1>"
+
+@app.route("/nombre/<string:nom>")
+def nombre(nom):
+    return "<h1> Hola </h1>" + nom
+
+@app.route("/numero/<int:n1>")
+def numero(n1):
+    return "<h1> El numero es {} <h1>".fromat(n1)
+
+@app.route("/user/<string:nom>/<int:id>")
+def user(nom,id):
+    return "<h1> ID {}, NOMBRE: {} <h1>".format(id,nom)
+
+@app.route("/suma/<float:n1>/<float:n2>")
+def suma(n1,n2):
+    return " La suma de {} + {} = {} ".format(n1,n2,n1+n2)
+
+@app.route("/mulitiplica",methods=["GET","POST"])
+def mult():
+        if request.method == "POST":
+            n1 = request.get["n1"]
+            n2 = request.get["n2"]
+            return  "<h1> El resultado es : {} </h1>".format(str(int(n1)*int(n2)))
+        else:
+            return '''
+                <form action="/mulitiplica" method="POST">
+                    <laber> Numero 1 </label>
+                    <input type="text" name="n1">
+                    <label> Numero 2 </label>
+                    <input type="text" name="n2">
+                    <input type="submit">
+                </form>
+'''
+@app.route("/formulario1")
+def calculo():
+    return render_template("formulario1.html")
+
+@app.route("/resultado",methods=["GET","POST"])
+def mult1():
+        if request.method == "POST":
+            n1 = request.form.get("n1")
+            n2 = request.form.get("n2")
+            return  "<h1> El resultado es : {} </h1>".format(str(int(n1)*int(n2)))
+
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
